10f6ece9e3cec0d0f36de26c4cdcf898a36e2056
DNL <danylo.zhalkovskyy@icloud.com>
2025-02-17 22:57:48 +0100

    show contactList at home

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,47 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
+import { useState } from 'react';
+
+import ContactList from '../src/components/ContactList/ContactList';
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const contacts = [
+    {
+      id: 1,
+      firstName: 'John',
+      lastName: 'Doe',
+      dateOfBirth: '1990-01-01',
+      phone: '1234567890',
+      email: 'john.doe@test.com',
+    },
+    {
+      id: 2,
+      firstName: 'Jane',
+      lastName: 'Doe',
+      dateOfBirth: '1995-01-01',
+      phone: '0987654321',
+      email: 'jane.doe@test.com',
+    },
+    {
+      id: 3,
+      firstName: 'James',
+      lastName: 'Smith',
+      dateOfBirth: '1992-01-01',
+      phone: '1234567890',
+      email: 'james.smith@test.com',
+    },
+  ];
+
+  setTimeout(() => {
+    setIsLoading(false);
+  }, 500);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <h1 className="text-3xl font-bold">Contact Book App</h1>
+      <ContactList contacts={contacts} isLoading={isLoading} />
     </>
   );
 }
