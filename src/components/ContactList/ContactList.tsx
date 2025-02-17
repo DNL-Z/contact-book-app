bf4b8b4515df219641aab73c812c4861f31690d7
DNL <danylo.zhalkovskyy@icloud.com>
2025-02-17 22:53:22 +0100

    created ContactList component

---
+++ src/components/ContactList/ContactList.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+
+import { Contact } from '../../types/contact.ts';
+import Spinner from '../Spinner/Spinner.tsx';
+
+interface Props {
+  contacts: Contact[];
+  isLoading: boolean;
+}
+
+const ContactList: React.FC<Props> = ({ contacts, isLoading }) => {
+
+  if (isLoading) return <Spinner />;
+  return (
+    <div className="grid grid-cols-3 gap-1">
+      {contacts.map((contact: Contact) => (
+        <div key={contact.id} className="max-w-sm rounded-2xl overflow-hidden bg-blue-200 m-7 p-7">
+          <div className="font-bold text-2xl text-gray-900 mb-4">{contact.firstName} {contact.lastName}</div>
+          <div className="text-gray-700">
+            <div>{contact.dateOfBirth}</div>
+            <div>{contact.email}</div>
+            <div>{contact.phone}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ContactList;
