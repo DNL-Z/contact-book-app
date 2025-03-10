4e3d9122284e7e9cf559796aa06250e6f58c72ce
DNL <danylo.zhalkovskyy@icloud.com>
2025-03-10 20:17:52 +0100

    changed ContactList and adding ContactItem component

--- src/components/ContactList/ContactList.tsx
+++ src/components/ContactList/ContactList.tsx
@@ -1,29 +1,31 @@
-import * as React from 'react';
+import React from 'react';
 
-import Spinner from '@/components/Spinner/Spinner';
-import { Contact } from '@/types/contact.ts';
+import ContactItem from '@/components/ContactItem/ContactItem.tsx';
+import { TContact } from '@/types/contact.ts';
 
 interface Props {
-  contacts: Contact[];
-  isLoading: boolean;
+  contacts: TContact[];
+  onPanel: (isNew: boolean, contact?: TContact) => void;
+  onDelete: (id: number) => void;
 }
 
-const ContactList: React.FC<Props> = ({ contacts, isLoading }) => {
-
-  if (isLoading) return <Spinner />;
+const ContactList: React.FC<Props> = ({ contacts, onPanel, onDelete }) => {
   return (
-    <div className="grid grid-cols-3 gap-1">
-      {contacts.map((contact: Contact) => (
-        <div key={contact.id} className="max-w-sm rounded-2xl overflow-hidden bg-blue-200 m-7 p-7">
-          <div className="font-bold text-2xl text-gray-900 mb-4">{contact.firstName} {contact.lastName}</div>
-          <div className="text-gray-700">
-            <div>{contact.dateOfBirth}</div>
-            <div>{contact.email}</div>
-            <div>{contact.phone}</div>
+    <>
+      <div
+        className="grid grid-cols-1 gap-3 rounded-2xl bg-blue-200 m-7 p-7 overflow-hidden overflow-y-auto h-146 w-146">
+        <button className="w-2/3 h-12 mx-auto" onClick={() => onPanel(true, undefined)}>New Contact</button>
+        {contacts.length > 0 ? contacts.map((contact: TContact) => (
+          <ContactItem key={contact.id} contact={contact} onPanel={() => onPanel(false, contact)}
+                       onDelete={onDelete} />
+        )) : (
+          <div className="text-center mt-10 mb-5">
+            <i className="fa-regular fa-clipboard fa-3x"></i>
+            <h2 className="text-gray-900 text-2xl mt-5">Contact Book is empty</h2>
           </div>
-        </div>
-      ))}
-    </div>
+        )}
+      </div>
+    </>
   );
 };
 
