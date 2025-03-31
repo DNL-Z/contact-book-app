a8dc03a1a8361be75cec6de2192c2a2a3fb25220
DNL <danylo.zhalkovskyy@icloud.com>
2025-04-01 00:07:44 +0200

    improvement of the component ContactItem

--- src/components/ContactItem/ContactItem.tsx
+++ src/components/ContactItem/ContactItem.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 
+import UseSidePanel from '@/hooks/useSidePanel.tsx';
+import useContactsStore from '@/stores/useContactsStore.ts';
 import { TContact } from '@/types/contact.ts';
 
 interface Props {
   contact: TContact;
-  onPanel: (isNew: boolean, contact?: TContact) => void;
-  onDelete: (id: string) => void;
 }
 
-const ContactItem: React.FC<Props> = ({ contact, onPanel, onDelete }) => {
+const ContactItem: React.FC<Props> = ({ contact }) => {
+  const { openPanel } = UseSidePanel();
+  const { deleteContact } = useContactsStore();
+
   return (
     <>
       <div key={contact.id} className="grid grid-cols-3 rounded-2xl max-h-36 bg-purple-50 p-3">
-        <div className="flex flex-col justify-center col-span-2">
+        <div className="flex flex-col justify-center col-span-2 truncate overflow-auto">
           <div className="font-bold text-2xl text-gray-900 mb-4">{contact.firstName} {contact.lastName}</div>
           <div className="text-gray-700">
             <div>{contact.dateOfBirth}</div>
@@ -21,9 +24,11 @@ const ContactItem: React.FC<Props> = ({ contact, onPanel, onDelete }) => {
           </div>
         </div>
         <div className="flex flex-col gap-3 justify-center">
-          <button className="bg-green-900 hover:bg-green-950" onClick={() => onPanel(false, contact)}>Edit Contact
+          <button className="bg-green-900 hover:bg-green-950" onClick={() => openPanel(false, contact)}>Edit Contact
           </button>
-          <button className="bg-red-900 hover:bg-red-950" onClick={() => onDelete(contact.id)}>Delete Contact</button>
+          <button className="bg-red-900 hover:bg-red-950" onClick={() => deleteContact(contact.id)}>Delete
+            Contact
+          </button>
         </div>
       </div>
     </>
